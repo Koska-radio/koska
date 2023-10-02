87fa5f0e6dcf1913d7a9052f58813051142657a9
Dan Bivol <danbivol@Dans-MacBook-Pro-2.local>
2023-10-02 17:31:57 +0200

    the latest build for deployment (beta)

--- src/PlayButton.jsx
+++ src/PlayButton.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Marquee from './Marquee';
 
 function PlayButton() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [metadata, setMetadata] = useState('CURRENTLY OFF-AIR');
   const audioRef = useRef(null);
 
   const handlePlayToggle = () => {
@@ -15,11 +14,7 @@ function PlayButton() {
     setIsPlaying(!isPlaying);
   };
 
-  useEffect(() => {
-    if (isPlaying) {
-      // Fetch current song metadata via API and update `metadata` state here
-    }
-  }, [isPlaying]);
+
 
   return (
     <div>
@@ -27,7 +22,7 @@ function PlayButton() {
         {isPlaying ? '❚❚' : '▶'}
       </button>
       <audio ref={audioRef} src="http://172.232.62.146:8000/radio.mp3" />
-      <Marquee metadata={metadata} />
+      <Marquee/>
     </div>
   );
 }
