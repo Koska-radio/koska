1b7ed89c79350f1951bdfb49b0199ae3d2fe4401
koska-radio <daniel@koskaradio.net>
2023-11-02 14:57:40 +0100

    Koska 3.0

--- src/Spline.jsx
+++ src/Spline.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 import SplineReact from '@splinetool/react-spline';
 
 const Spline = () => {
+  // This state will keep track of whether the window width is greater than 1000px
+  const [isDesktop, setIsDesktop] = React.useState(window.innerWidth > 1000);
+
+  const updateMedia = () => {
+    setIsDesktop(window.innerWidth > 1000);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('resize', updateMedia);
+    // Remove the event listener on cleanup
+    return () => window.removeEventListener('resize', updateMedia);
+  });
+
   return (
-    <SplineReact scene="https://prod.spline.design/eVg9U18rRHkBV69n/scene.splinecode" />
+    <>
+      {isDesktop && (
+        <SplineReact scene="https://prod.spline.design/eVg9U18rRHkBV69n/scene.splinecode" />
+      )}
+    </>
   );
-}
+};
 
 export default Spline;
