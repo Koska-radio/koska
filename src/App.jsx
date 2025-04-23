46387168337f24c7677215f087836926d97c18d3
Rocco Monteiro <rmonteir@richmond.edu>
2025-04-23 18:01:11 +0100

    Updated pages with latest build

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import "./style.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+///import { Link } from 'react-router-dom';
 import Home from "./Home"
 import Gallery from "./Gallery"
-import TrackArchives from "./TrackArchives"
+import Archive from "./Archive"
 
 function App() {
   return (
   <BrowserRouter>
-        <nav>
+        {/* <nav>
           <Link to="/">Home</Link>
           <Link to="/Gallery">Gallery</Link>
           <Link to="/TrackArchives">TrackArchives</Link>
-        </nav>
+        </nav> */}
         
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Gallery" element={<Gallery />} />
-          <Route path="/TrackArchives" element={<TrackArchives />} />
+          <Route path="/Archive" element={<Archive />} />
         </Routes>
       </BrowserRouter>
   );
